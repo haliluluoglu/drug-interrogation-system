77e8f5d6c091adee2bb8c6ee0d70631b7c2fc24b
Etkin0 <etkin866@gmail.com>
2019-04-18 11:26:28 +0300

    eczane ve doktor için şifre kısmı yenilendi. hasta activty butonu rengi kaldırıldı. renk ve benzeri şeyleri en son konuşuruz

--- app/src/main/java/com/example/cihan/eczanebul/EczaneGirisActivity.java
+++ app/src/main/java/com/example/cihan/eczanebul/EczaneGirisActivity.java
@@ -13,7 +13,7 @@ public class EczaneGirisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eczane_giris);
 
-        Button eczBilGir = (Button)findViewById(R.id.eczaneGirisButon);
+        Button eczBilGir = (Button)findViewById(R.id.eczGirisButon);
         eczBilGir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
