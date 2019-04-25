b4adce5989cf4779070265540d4d5f34c00a5ac7
cihan43 <cihan_unal@outlook.com>
2019-04-25 13:56:05 +0300

    recyclerview eklendi

--- app/src/main/java/com/example/cihan/eczanebul/HastaActivity.java
+++ app/src/main/java/com/example/cihan/eczanebul/HastaActivity.java
@@ -3,12 +3,14 @@ package com.example.cihan.eczanebul;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class HastaActivity extends AppCompatActivity {
+    private static final String TAG = "HastaActivity";
     private float koordx,koordy;
     private ArrayList<String> list = null;
     private ArrayList<ArrayList<String>> list2 = null;
@@ -20,6 +22,8 @@ public class HastaActivity extends AppCompatActivity {
         database = Database.getInstance(HastaActivity.this);
         koordx=1000.0f*(float)Math.random();
         koordy=1000.0f*(float)Math.random();
+
+        Log.d(TAG, "onCreate: x = "+koordx+" y = "+koordy);
 
         Button eczBul = (Button)findViewById(R.id.eczaneBul);
         Button ilacDisiBul = (Button)findViewById(R.id.ilacDisiBul);
